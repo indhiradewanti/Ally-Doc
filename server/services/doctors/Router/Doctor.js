8637cc5279ed8b3ab9f5e201c841cc675f2bd356
Rafi <rafipratama876@gmail.com>
2021-08-21 03:13:44 +0700

    ubah lagi

--- server/services/doctors/Router/Doctor.js
+++ server/services/doctors/Router/Doctor.js
@@ -7,8 +7,8 @@ router.get('/:_id', ControllerDoctor.getIdDoctor)
 router.post('/', upload.single("image"), ControllerDoctor.createDoctor)
 router.post('/login', ControllerDoctor.loginDoctor)
 router.put('/:_id', upload.single("image"), ControllerDoctor.updateDoctor)
-router.patch('/:_id', ControllerDoctor.patchStatus)
-router.patch('/:_id', upload.single("image"), ControllerDoctor.patchPhoto)
+router.patch('/status/:_id', ControllerDoctor.patchStatus)
+router.patch('/photo/:_id', upload.single("image"), ControllerDoctor.patchPhoto)
 router.delete('/:_id', ControllerDoctor.deleteDoctor)
 
 module.exports = router
