dcb1209c2cfa74a6edae39c5e0590e76acd7a3d2
wilsonangara <wilsonangara@gmail.com>
2021-08-22 01:43:23 +0700

    -

--- server/services/doctors/Config/index.js
+++ server/services/doctors/Config/index.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose')
-const db = mongoose.connection
+const mongoose = require("mongoose");
+const db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true, dbName: 'AllyDoc'}, () => {
-    console.log('connect to db')
-})
+mongoose.connect(
+	process.env.DB_ATLAS,
+	{ useNewUrlParser: true, useUnifiedTopology: true, dbName: "AllyDoc" },
+	() => {
+		console.log("connect to db");
+	}
+);
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', () => {
-    console.log('succesfully connected')
-})
+db.once("open", () => {
+	console.log("succesfully connected");
+});
 
-module.exports = mongoose
+module.exports = mongoose;
